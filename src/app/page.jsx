91e516af4c6fd1c11d5bb0d2e59151d25b5c9031
rishi <rishi18december@gmail.com>
2024-01-26 00:42:22 +0530

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="mainContainer">
+        Main vision: Aspire Access aims to build a multi services platform targetting three main stake holders i.e Students, job seekers and industries. It aims to provide one stop solution for all educational and opportunity solutions at a single platform.
+
+        At Aspire Access, our vision is to create a world where education and career opportunities are not determined by geographical or socio-economic factors. We believe that everyone deserves a chance to aspire and achieve, and we are dedicated to providing the tools and support needed to turn aspirations into accomplishments. With Aspire Access, your journey from learning to career success begins here.
+
+        We are building a first ever ecosystem to be able to solve the problems of growing individially in this competitive world. Helping industries, students, colleges, schools, students and other individuals to grow themselves by providing ocean of oppurtunities and awareness.
+      </div>
+
+    </>
+  );
+}
